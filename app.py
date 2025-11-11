8c58db4dcbf696fd58c472f7120729fb5b8cc298
milhaemmapas <milhaemmapas@gmail.com>
2025-11-10 21:39:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1137,7 +1137,7 @@ with tab_map["🏗️ Painel de Obras"]:
                         paper_bgcolor='rgba(0,0,0,0)',
                         font=dict(color='#2c3e50'),
                         showlegend=False,
-                        height=450
+                        height=480
                     )
                     
                     fig_ano.update_traces(
